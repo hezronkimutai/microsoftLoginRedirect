b5f482371ce46f3418c02926d2712bd6c0d3fd28
hezzie <hezronchelimo.hc@gmail.com>
2020-10-04 11:32:01 +0200

    used the localstorage to determine if the user is logged IN

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { login as loginFn, app, logout, getToken } from "./services/auth.service";
 import { getUserInfo } from "./services/graph.service";
@@ -34,34 +33,29 @@ export default () => {
   const login = () => {
     setLoginFailed(false);
     loginFn();
-    getToken();
+    // getToken();
   };
   useEffect(() => {
     app.handleRedirectCallback((error, response) => {
-      // handle redirect response or error
-      console.log("DDDDDDDDDDDDDDDDDD", response);
-      setUser(response);
+      if (error) setLoginFailed(error);
+      if (response) setUser(response);
     });
   }, []);
   let templates = [];
-  user && templates.push(<LoggedIn callAPI={callAPI} userName={user.name} />);
-  !user && templates.push(<LoggedOut login={login} />);
+  localStorage.getItem("msal.idtoken") &&
+    templates.push(<LoggedIn key="loggedIn" callAPI={callAPI} userName={user && user.account.name} />);
+  !localStorage.getItem("msal.idtoken") && templates.push(<LoggedOut key="loggedout" login={login} />);
   userInfo && templates.push(<pre key="userInfo">{JSON.stringify(userInfo, null, 4)}</pre>);
   loginFailed && templates.push(<strong key="loginFailed">Login unsuccessful</strong>);
   apiCallFailed && templates.push(<strong key="apiCallFailed">Graph API call unsuccessful</strong>);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">React app with MSAL.js</h1>
-      </header>
-      {templates}
-    </div>
-  );
+
+  console.log("#######################################");
+
+  return <div className="App">{templates}</div>;
 };
 
 const LoggedIn = ({ userName, callAPI }) => (
-  <div key="loggedIn">
+  <div>
     <button onClick={callAPI} type="button">
       Call Graph's /me API
     </button>
@@ -73,9 +67,11 @@ const LoggedIn = ({ userName, callAPI }) => (
 );
 
 const LoggedOut = ({ login }) => (
-  <div key="loggedout">
+  <div>
     <button onClick={login} type="button">
       Login with Microsoft
     </button>
   </div>
 );
+
+// https://www.youtube.com/watch?v=vlpwGQiDhP8
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,21 +1,18 @@
 import * as Msal from "msal";
 
-let PROD_REDIRECT_URI = "http://localhost:4000/";
-let redirectUri = window.location.origin;
-if (window.location.hostname !== "127.0.0.1") {
-  redirectUri = PROD_REDIRECT_URI;
-}
-
 const msalConfig = {
   auth: {
     clientId: "261d3e9d-08d5-462f-9982-930d3de1eaae",
     redirectUri: "http://localhost:3000/",
   },
   scopes: ["user.read", "mail.send"],
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: false,
+  },
 };
 
 export const app = new Msal.UserAgentApplication(msalConfig);
-let username = "";
 
 export const login = () => {
   return app.loginRedirect(msalConfig.scopes);
@@ -26,7 +23,6 @@ export const logout = () => {
 export const getToken = () => {
   return app.acquireTokenSilent(msalConfig).then(
     (accessToken) => {
-      console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", accessToken);
       return accessToken;
     },
     (error) => {
